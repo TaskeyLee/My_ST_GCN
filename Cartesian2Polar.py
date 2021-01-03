7f9710c38f05589ba0348771bdf60f1dabaaa10c
Taskey <1524071136@qq.com>
2021-01-03 14:54:43 +0800

    目前实现了两种坐标转换-归一化方法：相对中心点+极坐标；相对向心节点+极坐标，并再model.py中进行实验，比较不同坐标表示方法对模型效果的影响

--- Cartesian2Polar.py
+++ Cartesian2Polar.py
@@ -16,7 +16,7 @@ import numpy as np
 # Right Hip: 12
 # Right Knee: 13
 # Right Ankle：14
-train_tensor, train_label = torch.load('../dataset/train.pkl') # 导入数据集
+# train_tensor, train_label = torch.load('../dataset/train.pkl') # 导入数据集
 # 原始数据集的关节点坐标都为世界坐标系，数值很大且分布分散
 # 所以定义一个class，用于将世界笛卡尔坐标系转为相对center的极坐标系，其中选spine为center
 class Cartesian2Polar():
@@ -25,39 +25,70 @@ class Cartesian2Polar():
         if dataset.type() == 'torch.DoubleTensor' or dataset.type() == 'torch.IntTensor':
             self.dataset = dataset.numpy() # tensor转numpy，便于操作
             
-    def coordinate2distance(self): # 计算每个关节点相对center的欧氏距离
-        normalized_train_numpy = []
-        for train_data in self.dataset: # 遍历所有数据
-            normalized_train_data = []
-            for body_points in train_data: # 每个train_data中有32帧的关节点三维坐标信息，遍历每一帧
+    def Center_Polar(self): # 计算每个关节点相对center的极坐标
+        normalized_distance = []
+        normalized_angle = [] 
+        for data in self.dataset: # 遍历所有数据
+            normalized_data_distance = []
+            normalized_data_angle = []
+            for body_points in data: # 每个train_data中有32帧的关节点三维坐标信息，遍历每一帧
                 center = body_points[2] # 把spine作为人体中心
-                normalized_body_points = []
+                normalized_body_points_distance = []
+                normalized_body_points_angle = []
                 distance_max = 0
-                for points in body_points: # 遍历一帧中的每个关节点
+                for idx, points in enumerate(body_points):  # 遍历一帧中的每个关节点
+                    # 求节点间距离
                     distance = np.sqrt(np.sum(np.square(points - center))) # 求每个关节点到center的欧氏距离
                     if distance > distance_max: # 求得这一组距离中的最大值，用于后续归一化
                         distance_max = distance
-                        
-                    normalized_body_points.append(distance) # 把当前关节点相对center的坐标存入
-                normalized_body_points /= distance_max # 归一化处理
-                normalized_train_data.append(normalized_body_points) # 把15个关节点的相对归一化坐标存入（15关节点组成一帧）
-            normalized_body_points = np.array(normalized_body_points)
-            # normalized_train_data = np.array(normalized_train_data)
-           
-            normalized_train_numpy.append(normalized_train_data) # 把32帧的关节点相对归一化坐标存入（32帧组成一个视频）
-        normalized_train_numpy = np.array(normalized_train_numpy)
+                    
+                    # 求节点间角度
+                    if idx != 2: 
+                        # angle_xoy = np.arctan((points[2] - center[2])/np.sqrt(np.square(points[1] - center[1]) + np.square(points[0] - center[0])))
+                        # angle_xoz = np.arctan((points[1] - center[1])/np.sqrt(np.square(points[0] - center[0]) + np.square(points[2] - center[2])))
+                        # angle_yoz = np.arctan((points[0] - center[0])/np.sqrt(np.square(points[1] - center[1]) + np.square(points[2] - center[2])))   
+                        angle_1 = np.arctan((points[0] - center[0]) / (points[1] - center[1]))
+                        angle_2 = np.arctan((points[2] - center[2])/np.sqrt(np.square(points[0] - center[0]) + np.square(points[1] - center[1])))
+                    else:
+                        angle_1 = angle_2 = 0
+                    
+        # 保存distance信息与angle信息
+                    normalized_body_points_distance.append(distance) # 把当前关节点相对center的坐标存入
+                    normalized_body_points_angle.append(np.array((angle_1, angle_2))) # 把当前关节点相对center的角度存入
+                
+                normalized_body_points_distance /= distance_max # 归一化处理
+                normalized_data_distance.append(normalized_body_points_distance) # 把15个关节点组成的一帧的相对归一化坐标存入（15关节点组成一帧）
+                normalized_data_angle.append(normalized_body_points_angle) # 把15个关节点的相对归一化坐标存入（15关节点组成一帧）
+            
+            normalized_distance.append(normalized_data_distance) # 把32帧的关节点相对归一化坐标存入（32帧组成一个视频）
+            normalized_angle.append(normalized_data_angle) # 把32帧的关节点相对角度存入（32帧组成一个视频）
         
-        normalized_train_tensor = torch.tensor(normalized_train_numpy) # 把numpy重新转为tensor供神经网络使用
-        return normalized_train_tensor
-     
-    def coordinate2angle(self): # 计算每个关节点相对center的两个angle
-        normalized_train_numpy = [] 
-        for train_data in self.dataset: # 遍历所有数据
-            normalized_train_data = []
-            for body_points in train_data: # 每个train_data中有32帧的关节点三维坐标信息，遍历每一帧
-                center = body_points[2] # 把spine作为人体中心
-                normalized_body_points = []
+        normalized_distance = np.array(normalized_distance)
+        normalized_angle = np.array(normalized_angle)
+        
+        normalized_distance_tensor = torch.tensor(normalized_distance) # 把numpy重新转为tensor供神经网络使用
+        normalized_angle_tensor = torch.tensor(normalized_angle) # 把numpy重新转为tensor供神经网络使用
+        
+        return normalized_distance_tensor, normalized_angle_tensor
+
+    
+    def Relative_Polar(self): # 转换为相对向心节点的极坐标
+        dic = {0:1, 3:1, 6:1, 4:3, 5:4, 7:6, 8:7, 2:2, 1:2, 9:2, 12:2, 10:9, 11:10, 13:12, 14:13}
+        normalized_angle = [] 
+        normalized_distance = [] 
+        for train_data in self.dataset: # 遍历所有172个视频数据
+            normalized_data_distance = []
+            normalized_data_angle = []
+            for idx, body_points in enumerate(train_data): # 每个视频有32帧，遍历
+                distance_max = 0
+                normalized_body_points_distance = []
+                normalized_body_points_angle = []
                 for idx, points in enumerate(body_points): # 遍历一帧中的每个关节点
+                    center = body_points[dic[idx]] # 把spine作为人体中心
+                    # 求节点间距离
+                    distance = np.sqrt(np.sum(np.square(points - center))) # 求每个关节点到center的欧氏距离
+                    if distance > distance_max: # 求得这一组距离中的最大值，用于后续归一化
+                        distance_max = distance
                     # 求得当前关节点与center再三维坐标系中的相对角度
                     if idx != 2:
                         # angle_xoy = np.arctan((points[2] - center[2])/np.sqrt(np.square(points[1] - center[1]) + np.square(points[0] - center[0])))
@@ -67,18 +98,26 @@ class Cartesian2Polar():
                         angle_2 = np.arctan((points[2] - center[2])/np.sqrt(np.square(points[0] - center[0]) + np.square(points[1] - center[1])))
                     else:
                         angle_1 = angle_2 = 0
-                    normalized_body_points.append(np.array((angle_1, angle_2))) # 把当前关节点相对center的角度存入
-                normalized_train_data.append(normalized_body_points) # 把15个关节点的相对归一化坐标存入（15关节点组成一帧）
-            normalized_body_points = np.array(normalized_body_points)
-            # normalized_train_data = np.array(normalized_train_data)
+                        
+                    normalized_body_points_distance.append(distance) # 把当前关节点相对center的坐标存入
+                    normalized_body_points_angle.append(np.array((angle_1, angle_2))) # 把当前关节点相对center的角度存入
+                
+                normalized_body_points_distance /= distance_max # 归一化处理
+                normalized_data_distance.append(normalized_body_points_distance) # 把15个关节点组成的一帧的相对归一化坐标存入（15关节点组成一帧）
+                normalized_data_angle.append(normalized_body_points_angle) # 把15个关节点的相对归一化坐标存入（15关节点组成一帧）
            
-            normalized_train_numpy.append(normalized_train_data) # 把32帧的关节点相对归一化坐标存入（32帧组成一个视频）
-        normalized_train_numpy = np.array(normalized_train_numpy)
+            normalized_distance.append(normalized_data_distance) # 把32帧的关节点相对归一化坐标存入（32帧组成一个视频）
+            normalized_angle.append(normalized_data_angle) # 把32帧的关节点相对角度存入（32帧组成一个视频）
         
-        normalized_train_tensor = torch.tensor(normalized_train_numpy) # 把numpy重新转为tensor供神经网络使用
-        return normalized_train_tensor
+        normalized_distance = np.array(normalized_distance)
+        normalized_angle = np.array(normalized_angle)
+        
+        normalized_distance_tensor = torch.tensor(normalized_distance) # 把numpy重新转为tensor供神经网络使用
+        normalized_angle_tensor = torch.tensor(normalized_angle) # 把numpy重新转为tensor供神经网络使用
+        return normalized_distance_tensor, normalized_angle_tensor
 
 
-coordinate_normalization = Cartesian2Polar(train_tensor)
-normalized_polar_distance = coordinate_normalization.coordinate2distance()
-normalized_polar_angle = coordinate_normalization.coordinate2angle()
+# coordinate_normalization = Cartesian2Polar(train_tensor)
+# normalized_polar_distance = coordinate_normalization.coordinate2distance()
+# normalized_polar_angle = coordinate_normalization.coordinate2angle()
+# relative_polar = coordinate_normalization.relative_polar()
